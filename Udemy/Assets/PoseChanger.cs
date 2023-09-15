202f7df5361b6bb172a49cc64ebd1e635a50f676
kimhyeok <kimmang940715@naver.com>
2023-09-15 14:33:23 +0900

    PoseChanger 스크립트 추가

---
+++ Udemy/Assets/PoseChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim = null;
+    void Start()
+    {
+        anim = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
